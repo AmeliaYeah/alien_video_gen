a4c8b8e2942e8ffc98962e1252d56a1822d7589c
bruh <87606015+AmeliaYeah@users.noreply.github.com>
2023-01-26 19:55:13 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+dest_f_dir="PUT THE DIRECTORY WHERE YOU WANT YOUR VIDEOS TO GO"
+
 for i in `seq 300`
 do
     python3 gen.py $(faker word | tr -d '\n') $(faker word | tr -d '\n')
     rand=$(head -c 5 /dev/urandom | xxd -u -ps)
-    mv final.mp4 "/tmp/tmp.iwG5YW2VYA/${rand}${i}.mp4"
+    mv final.mp4 "${dest_f_dir}/${rand}${i}.mp4"
 done
